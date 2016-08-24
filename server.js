bdfacb4753f1381008adb0d9c1e4a868e4d85da4
Michael Vines <mvines@silklabs.com>
2016-08-24 14:56:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post('/tableflip', function (req, res) {
 
   payload = {
     channel: channel,
-    response_type: "in_channel',
+    response_type: 'in_channel',
     text: '(╯°□°）╯︵ ' + text,
     icon_emoji: ':rage1:'
   };
